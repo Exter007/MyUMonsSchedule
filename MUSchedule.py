327526c1d286e20a79d8fbf32b8ff50113e2c7a8
Nephty <72945143+Nephty@users.noreply.github.com>
2021-11-19 14:27:17 +0100

    Changed path to login file (#1)

--- MUSchedule.py
+++ MUSchedule.py
@@ -26,7 +26,7 @@ Sender_Email = ""
 Reciever_Email = ""
 
 # Fichier config
-config_file = open(f"{cwd}/config/{login_file_name}.txt", "r")
+config_file = open(f"./config/{login_file_name}.txt", "r")
 # Adresse mail UMons (matricule@umons.ac.be)
 user = config_file.readline().strip()
 # Mot de passe UMons
