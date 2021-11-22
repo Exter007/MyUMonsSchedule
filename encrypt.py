37667373b24c2d6e412e181f758b6a8ab69827f0
Nephty <72945143+Nephty@users.noreply.github.com>
2021-11-22 15:22:07 +0100

    Added docstrings

--- encrypt.py
+++ encrypt.py
@@ -4,6 +4,10 @@ from cryptography.hazmat.primitives import serialization, hashes
 
 
 def generate_private_key():
+    """
+    Generates a private key
+    :return: The private key
+    """
     return rsa.generate_private_key(
         public_exponent=65537,
         key_size=2048,
@@ -12,29 +16,59 @@ def generate_private_key():
 
 
 def generate_public_key(private_key):
+    """
+    Generate a public key linked to the given private key
+    :param private_key: The original private key
+    :return: The public key generated from the private key
+    """
     return private_key.public_key()
 
 
 def save_private_key(file, key):
-    with open(file, 'wb') as f:
-        f.truncate()
-        f.write(key.private_bytes(
-            encoding=serialization.Encoding.PEM,
-            format=serialization.PrivateFormat.PKCS8,
-            encryption_algorithm=serialization.NoEncryption()
-        ))
+    """
+    Writes the given private key to the given file
+    :param file: The destination file
+    :param key: The private key to save
+    :return: True if the key has been saved, False otherwise
+    """
+    try:
+        with open(file, 'wb') as f:
+            f.truncate()
+            f.write(key.private_bytes(
+                encoding=serialization.Encoding.PEM,
+                format=serialization.PrivateFormat.PKCS8,
+                encryption_algorithm=serialization.NoEncryption()
+            ))
+        return True
+    except:
+        return False
 
 
 def save_public_key(file, key):
-    with open(file, 'wb') as f:
-        f.truncate()
-        f.write(key.public_bytes(
-            encoding=serialization.Encoding.PEM,
-            format=serialization.PublicFormat.SubjectPublicKeyInfo
-        ))
+    """
+    Writes the given public key to the given file
+    :param file: The destination file
+    :param key: The public key to save
+    :return: True if the key has been saved, False otherwise
+    """
+    try:
+        with open(file, 'wb') as f:
+            f.truncate()
+            f.write(key.public_bytes(
+                encoding=serialization.Encoding.PEM,
+                format=serialization.PublicFormat.SubjectPublicKeyInfo
+            ))
+        return True
+    except:
+        return False
 
 
 def read_private_key(file):
+    """
+    Read the private key stored in the given file
+    :param file: The source file
+    :return: The private key read from the file
+    """
     with open(file, "rb") as key_file:
         return serialization.load_pem_private_key(
             key_file.read(),
@@ -44,6 +78,11 @@ def read_private_key(file):
 
 
 def read_public_key(file):
+    """
+    Read the public key stored in the given file
+    :param file: The source file
+    :return: The public key read from the file
+    """
     with open(file, "rb") as key_file:
         return serialization.load_pem_public_key(
             key_file.read(),
@@ -51,9 +90,15 @@ def read_public_key(file):
         )
 
 
-def encrypt(message, public_key):
+def encrypt(data, public_key):
+    """
+    Encrypts the given string using the given public key and returns the encrypted data
+    :param data: The data to encrypt
+    :param public_key: The public key to use
+    :return: The encrypted data
+    """
     return public_key.encrypt(
-        message,
+        data,
         padding.OAEP(
             mgf=padding.MGF1(algorithm=hashes.SHA256()),
             algorithm=hashes.SHA256(),
@@ -63,6 +108,12 @@ def encrypt(message, public_key):
 
 
 def decrypt(encrypted, private_key):
+    """
+    Decrypts the given encrypted data using the given private key and returns the decrypted data
+    :param encrypted: The encrypted data to decrypt
+    :param private_key: The private key to use
+    :return: The decrypted data
+    """
     return private_key.decrypt(
         encrypted,
         padding.OAEP(
